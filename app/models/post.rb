a18c27ff31efab24f72a4136aa790309e7e01a89
Adam Braus <ajbraus@gmail.com>
2016-12-12 17:30:45 -0800

    voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments
+  
   has_many :votes
   has_many :voters, through: :votes, source: "user"
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_many :posts
+
   has_many :votes
   has_many :voted_posts, through: :votes, source: "post"
+  # has_many :upvoted_posts, through: :votes, source: "post", -> { where value: 1 }
+  # has_many :downvoted_posts, through: :votes, source: "post", -> { where value: -1 }
   
   has_secure_password
 
